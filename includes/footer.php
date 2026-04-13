c82a9ebd0c5595c3b00b4cccf9304ada73a35dc5
unknown <sitizulfatulhajar@gmail.com>
2026-04-13 09:46:59 +0700

    Add Footer

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="footer-containers">
+        <div class="footer-cards">
+
+            <div class="footer-yapping">
+                <h2>Ra-An-Nabil</h2>
+                <p>Layanan informasi dan pendaftaran PPDB TK yang membantu orang tua dalam <br> mengakses data sekolah dan proses pendaftaran dengan mudah dan jelas.</p>
+            </div>
+
+            <div class="footer-menu">
+                <h2>Menu</h2>
+                <a href="#beranda">Beranda</a>
+                <a href="#profile">Profile</a>
+                <a href="#berita">Berita</a>
+                <a href="daftar.php">PPDB</a>
+            </div>
+
+            <div class="footer-sosial">
+                <h2>Contact</h2>
+                <ul>
+                    <li>
+                        <a href="#"><i class="fab fa-whatsapp"></i>Whatsapp</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-facebook"></i>Facebook</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fas fa-envelope"></i>Email</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+         <div class="copyright">
+            <hr>
+            <p>© 2026 Ra-An-Nabil. Sistem PPDB TK terpercaya untuk masa depan anak.</p>
+        </div>
+
+    </div>
+
+   
+</footer>
