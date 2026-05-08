52593145985d6aa947e7336b8c7e57f724c2db42
unknown <rafka>
2026-05-08 11:28:10 +0700

    upd koneksi db

--- config/database.php
+++ config/database.php
@@ -1,7 +1,21 @@
-<?php 
-$conn = mysqli_connect('localhost', 'root', '', 'ppdb_tk_an_nabil');
+<?php
 
+$host     = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'ppdb_tk_an_nabil';
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Cek koneksi
 if (!$conn) {
-    echo "Koneksi Ke Dalam Database Gagal!";
+    // Simpan error asli ke log server
+    error_log('Database connection failed: ' . mysqli_connect_error());
+
+    // Pesan aman untuk user
+    die('Koneksi database gagal.');
 }
+
+// Set charset agar lebih aman dari encoding issue
+mysqli_set_charset($conn, 'utf8mb4');
 ?>
